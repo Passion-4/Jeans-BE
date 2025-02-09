a52d1e2476680789dec9e41e1dfa01207a92d87c
Soyeon Cha <89539031+Soyeon-Cha@users.noreply.github.com>
2025-02-10 05:52:00 +0900

    feat: 초기 세팅

---
+++ src/main/java/Jeans/Jeans/JeansApplication.java
@@ -0,0 +1,13 @@
+package Jeans.Jeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Jeans/Jeans/JeansApplicationTests.java
@@ -0,0 +1,13 @@
+package Jeans.Jeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
